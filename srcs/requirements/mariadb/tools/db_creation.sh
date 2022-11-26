b9f6d286bb234b40e5eafe1de83b429af46511f6
ishakzail <ishak.zail.iz@gmail.com>
2022-11-26 19:33:05 +0100

    update : change script to prevent server exit()

--- srcs/requirements/mariadb/tools/db_creation.sh
+++ srcs/requirements/mariadb/tools/db_creation.sh
@@ -1,13 +1,16 @@
-#! /bin/sh
-# Database creation
-
-
-    service mysql start    
+#! /bin/bash
+# Database creation 
+    service mysql start
+# if ! ls /var/lib/mysql/*; then
     echo "Lanch DATABASE Creation";
-    mysql -u root  -e "CREATE DATABASE test";
-    mysql -u root  -e "CREATE USER ishak@'%' IDENTIFIED BY 'toor'";
-    mysql -u root  -e "GRANT ALL PRIVILEGES ON test.* TO 'ishak'@'%'";
-    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'toor';";
-    mysql -u root -e "FLUSH PRIVILEGES;";
+    mysql -u root  -e "CREATE DATABASE IF NOT EXISTS izail_db";
+    mysql -u root  -e "CREATE USER IF NOT EXISTS 'ishak'@'%' IDENTIFIED BY 'toor'";
+    mysql -u root  -e "GRANT ALL PRIVILEGES ON *.* TO 'ishak'@'%'";
+    mysql -u root -e "FLUSH PRIVILEGES";
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'toor1'";
+    mysql -u root -e "FLUSH PRIVILEGES";
+# fi
+
+kill `cat /var/run/mysqld/mysqld.pid`
 
 mysqld_safe;
