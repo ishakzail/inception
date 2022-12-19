1a900f398213f52c501bc715ca10cd15cb5bfb0b
ishakzail <ishak.zail.iz@gmail.com>
2022-12-19 16:05:51 +0100

    error : script need to be fixed

--- srcs/requirements/mariadb/tools/db_creation.sh
+++ srcs/requirements/mariadb/tools/db_creation.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
 # Database creation 
     service mysql start
+    # service mysql stop
 # if ! ls /var/lib/mysql/*; then
     echo "Lanch DATABASE Creation";
-    mysql -u root  -e "CREATE DATABASE IF NOT EXISTS izail_db";
-    mysql -u root  -e "CREATE USER IF NOT EXISTS 'ishak'@'%' IDENTIFIED BY 'toor'";
-    mysql -u root  -e "GRANT ALL PRIVILEGES ON *.* TO 'ishak'@'%'";
-    mysql -u root -e "FLUSH PRIVILEGES";
-    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'toor1'";
-    mysql -u root -e "FLUSH PRIVILEGES";
+    # mysql -u root  -e "CREATE DATABASE IF NOT EXISTS izail_db";
+    # mysql -u root  -e "CREATE USER IF NOT EXISTS 'ishak'@'%' IDENTIFIED BY 'toor'";
+    # mysql -u root  -e "GRANT ALL PRIVILEGES ON izail_db.* TO 'ishak'@'%' WITH GRANT OPTION";
+    # mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'toor1'";
+    # mysql -u root -e "FLUSH PRIVILEGES";
 # fi
 
-kill `cat /var/run/mysqld/mysqld.pid`
+    # service mysql stop
 
-mysqld_safe;
+# kill `cat /var/run/mysqld/mysqld.pid`
+
+mysqld;
