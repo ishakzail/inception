41fb7ab340b0c32e8a989e3a4f83ec57fd05d926
ishakzail <ishak.zail.iz@gmail.com>
2022-12-25 19:40:04 +0100

    update: change WP installation procces to wp-cli

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,37 +1,21 @@
 #!/bin/bash
 
-# Download and extract WordPress
-# mkdir -p /var/www/html/
-# cd /var/www/html/ && wget https://wordpress.org/latest.tar.gz
-# tar -xzf latest.tar.gz
-# rm latest.tar.gz
-# # cp -R wordpress /var/www/html/
-# chown -R www-data:www-data /var/www/html/wordpress/
-# chmod -R 755 /var/www/html/wordpress/
-# mv /var/www/html/wordpress/* ./ && rm -rf wordpress
-
-
-
-
-# mkdir /var/www/html/wordpress/wp-content/uploads
-# chown -R www-data:www-data /var/www/html/wordpress/wp-content/uploads/
-
-#Create a MySQL database and user for WordPress
-# mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress";
-# mysql -u root -e "CREATE USER  IF NOT EXISTS 'wordpress'@'%' IDENTIFIED BY 'password'";
-# mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%'";
-# mysql -u root -e "FLUSH PRIVILEGES";
-
 # Configure WordPress
-rm -rf /var/www/html/wp-config-sample.php 
+echo "name ==";
+echo $DB_NAME;
+echo "Here";
 
-sed -i "s/database_name_here/wordpress/g" /var/www/html/wp-config.php
-sed -i "s/username_here/izail/g" /var/www/html/wp-config.php
-sed -i "s/password_here/izail1337/g" /var/www/html/wp-config.php
-sed -i "s/localhost/mariadb/g" /var/www/html/wp-config.php
+wp core download --path=/var/www/html --allow-root
+rm -rf /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+chown -R www-data:www-data /var/www/html && chmod -R 755 /var/www/html
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER_NAME --dbpass=$DB_USER_PASS --dbhost=$HOST --path=/var/www/html/ --allow-root --skip-check --extra-php
+
+wp core install --url=izail.42.fr --title=Example --admin_user=ishak --admin_password=123456 --admin_email=izail@gmail.com --path=/var/www/html/ --allow-root
+wp user create izail user@gmail.com --role=author --user_pass=123456 --allow-root --path=/var/www/html/
+
+# the file /var/run/php/ will be created automatically when you run php service
 service php7.3-fpm start
 service php7.3-fpm stop
 # tail -f /dev/null
 # /usr/sbin/php-fpm;
 php-fpm7.3 -F
-# service php7.3-fpm start
